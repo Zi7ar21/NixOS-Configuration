150325dda6f6294dab54d7987dd5e475a57fcb8c
Abbas G <abbas@abbasg.com>
2021-11-22 04:44:34 +0300

    System Update and change gnome3 to gnome

--- common/gnome.nix
+++ common/gnome.nix
@@ -12,7 +12,7 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.gnome3.gnome-keyring.enable = lib.mkForce false;
+  services.gnome.gnome-keyring.enable = lib.mkForce false;
   security.pam.services.lightdm.enableGnomeKeyring = false;
   programs.ssh.startAgent = true;
   environment.gnome.excludePackages = [ pkgs.gnome.cheese pkgs.gnome-photos pkgs.gnome.gnome-music pkgs.gnome.gedit pkgs.epiphany pkgs.evince pkgs.gnome.gnome-characters pkgs.gnome.totem pkgs.gnome.tali pkgs.gnome.iagno pkgs.gnome.hitori pkgs.gnome.atomix pkgs.gnome-tour ];
